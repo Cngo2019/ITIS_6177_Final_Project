e66432d95b837a7ed06cc8151f14c5cd08890177
can <canngo215@gmail.com>
2025-04-23 09:15:07 -0400

    add markdown

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import visionRoutes from "./routes/computervision.routes";
-import express from 'express';
+import express, {urlencoded} from 'express';
 import {errorHandler} from "./utils";
-
+import {html} from "./docs/docs"
 const app = express();
 const port = 3000;
-app.use('/api', visionRoutes);
+
+
 // ONLY apply JSON parser after file routes
 app.use(express.json());
+app.use(urlencoded({extended: true}));
 app.use(errorHandler);
+app.use('/api', visionRoutes);
+app.get('/docs', (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.send(html);
+})
+
 app.listen(port, () => {
     console.log(`Facial Analysis API is running at http://localhost:${port}`);
 });
