d90f74c440879d61913767efb9ad9ae95b9a225f
can <canngo215@gmail.com>
2025-04-26 15:41:51 -0400

    add navs and home pages

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -4,11 +4,11 @@ import * as path from "node:path";
 
 const router = Router();
 router.get("/describe", (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'public', 'describe.html'));
+    res.sendFile(path.join(process.cwd(), 'public/describe', 'describe.html'));
 });
 
 router.get("/remove-background", (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'public', 'remove-background.html'));
+    res.sendFile(path.join(process.cwd(), 'public/remove-background', 'remove-background.html'));
 });
 
 export default router;
